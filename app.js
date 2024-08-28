68265d1d5d3128b3c1ecab9567b2807f5a585993
Arslan Ali <muhammadarslanrajput12@gmail.com>
2024-08-28 12:13:31 +0500

    && oprater

---
+++ app.js
@@ -0,0 +1,48 @@
+// truthy values
+// falsy ky illawa sab turthy hyn
+
+// falsy values
+// 0
+// null
+// undefined
+// ""
+// NaN
+// false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// last truthy value is returned
+// console.log(1 && true && " " && 1);
+// console.log(false && 0 && true);
+// let test2 = true && "shehzad";
+// let test3 = "shehzad" && true;
+// let test4 = null && "shehzad" && 56;
+// let test = "shehzad" && null && 56;
+// let test5 = true && "shehzad" && 56;
+// let test6 = true && 0 && 56;
+// let test7 = "hh" && null;
+// let test8 = "hh" && "kk";
+
+
+  let array=[1,2,3,4,5]
+
+function home(){
+  
+   
+    document.write( "Hello World")
+}
+let num=array.includes(1);
+// let num=array.includes(12);
+
+num&&home()
